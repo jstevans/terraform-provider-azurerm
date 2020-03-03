ad28f1085c10c0adebba4b9ccc4df042c68dd33a
John Stevans <jstevans@users.noreply.github.com>
2020-03-02 16:13:16 -0800

    Test custom_headers for trafficmanagerprofile

--- azurerm/internal/services/trafficmanager/tests/resource_arm_traffic_manager_profile_test.go
+++ azurerm/internal/services/trafficmanager/tests/resource_arm_traffic_manager_profile_test.go
@@ -314,6 +314,11 @@ resource "azurerm_traffic_manager_profile" "test" {
       "100-101",
       "301-303",
     ]
+    
+    custom_headers {
+    	name = "foo"
+	value = "bar"
+    }
 
     protocol = "tcp"
     port     = 777
@@ -356,6 +361,11 @@ resource "azurerm_traffic_manager_profile" "test" {
     expected_status_code_ranges = [
       "302-304",
     ]
+    
+    custom_headers {
+    	name = "foo2"
+	value = "bar2"
+    }
 
     protocol = "https"
     port     = 442
